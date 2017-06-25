bf5f58f9080bd29afc2b5cfff648e27336b9cd47
MarceloCRibeiro <marcelocarvalhoribeiro@gmail.com>
2017-06-24 21:36:02 -0300

    Add files via upload

---
+++ BluSoft/pages/Cliente.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web.Services;
+
+using BluSoftService.Service;
+using BluSoftService.Repository;
+
+public partial class Cliente : System.Web.UI.Page
+{
+
+    #region Properties
+
+    
+    #endregion
+
+    #region Events
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    #endregion
+
+    #region Methods
+
+
+    #endregion
+
+    #region WebMethods
+
+    [WebMethod(EnableSession = true)]
+    public static bool CadastrarCliente(string cpf, string rg, string nome, string nascimento, string telefone, string uf)
+    {
+        
+        try
+        {
+            BluSoftService.Service.BluSoftService service = new BluSoftService.Service.BluSoftService();
+
+            var cliente = new BluSoftService.Repository.Cliente();
+            cliente.CPF = cpf;
+            cliente.RG = rg;
+            cliente.Nome = nome;
+            cliente.DataNascimento = Convert.ToDateTime(nascimento);
+            cliente.Telefone = telefone;
+            cliente.UF = uf;
+
+            return service.CadastrarCliente(cliente);
+
+        
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return true;
+
+    }
+
+    #endregion
+
+}
+
